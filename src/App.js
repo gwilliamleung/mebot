6cc18c2afc1e405f80d7baf771f38a1bdeb70cec
William Leung <gwilliamleung@gmail.com>
2023-05-27 17:20:30 -0400

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { FaNewspaper, FaArrowRight } from "react-icons/fa"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-slate-300 flex justify-center items-center h-screen">
+      <div className="bg-slate-500 w-1/3 h-2/3 justify-between rounded-md items-center flex flex-col">
+          <div className="flex w-full justify-between items-center p-8">
+            <FaNewspaper />
+            <div className="flex flex-col">
+              <h2 className="text-white">MeBot</h2>
+              <h2>Ask me anything!</h2>
+            </div>
+          </div>
+          <div className="h-full w-full bg-white justify-between rounded-b-md flex flex-col">
+            <div>Chats</div>
+            <form>
+              <div className="flex justify-between border-slate-700 border-2">
+                <input className="w-3/4"></input>
+                <FaArrowRight/>
+              </div>
+            </form>
+          </div>
+      </div>
+
     </div>
   );
 }
