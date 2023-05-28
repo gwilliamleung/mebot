da9bbcbd7a7c46f0acb134d40ecf1f03dea463a6
William Leung <gwilliamleung@gmail.com>
2023-05-28 18:14:25 -0500

    chats

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import { FaNewspaper, FaArrowRight } from "react-icons/fa"
+import React, { useState } from 'react'
+
 
 function App() {
+  const [userInput, setUserInput] = useState('')
+  const [userInputsArray, setUserInputsArray] = useState([])
+
+  const handleInputChange = (event) => {
+    setUserInput(event.target.value);
+    setUserInputsArray((prevUserInputsArray) => [
+      ...prevUserInputsArray,
+      event.target.value
+    ])
+  }
+
   return (
     <div className="bg-slate-300 flex justify-center items-center h-screen">
       <div className="bg-slate-500 w-1/3 h-2/3 justify-between rounded-md items-center flex flex-col">
@@ -13,12 +26,16 @@ function App() {
           </div>
           <div className="h-full w-full bg-white justify-between rounded-b-md flex flex-col">
             <div>Chats</div>
-            <form>
               <div className="flex justify-between border-slate-700 border-2">
-                <input className="w-3/4"></input>
-                <FaArrowRight/>
+                <input 
+                type = "text"
+                value={userInput}
+                className="w-3/4"/>
+                <button onClick={handleInputChange}><FaArrowRight/></button>
+
+                {userInputsArray.map((input,index)=>
+                <div key={index}>{input}</div>)}
               </div>
-            </form>
           </div>
       </div>
 
