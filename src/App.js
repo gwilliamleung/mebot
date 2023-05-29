24779db5144818a0cea1db47dc1a5833fbf9120b
William Leung <gwilliamleung@gmail.com>
2023-05-29 16:39:17 -0400

    chat divs

--- src/App.js
+++ src/App.js
@@ -8,37 +8,48 @@ function App() {
 
   const handleInputChange = (event) => {
     setUserInput(event.target.value);
-    setUserInputsArray((prevUserInputsArray) => [
-      ...prevUserInputsArray,
-      event.target.value
-    ])
-  }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (userInput.trim() !== '') {
+      setUserInputsArray((prevUserInputsArray) => [
+        ...prevUserInputsArray,
+        userInput,
+      ]);
+      setUserInput('');
+    }
+  };
 
   return (
     <div className="bg-slate-300 flex justify-center items-center h-screen">
       <div className="bg-slate-500 w-1/3 h-2/3 justify-between rounded-md items-center flex flex-col">
-          <div className="flex w-full justify-between items-center p-8">
-            <FaNewspaper />
-            <div className="flex flex-col">
-              <h2 className="text-white">MeBot</h2>
-              <h2>Ask me anything!</h2>
-            </div>
+        <div className="flex w-full justify-between items-center p-8">
+          <FaNewspaper />
+          <div className="flex flex-col">
+            <h2 className="text-white">MeBot</h2>
+            <h2>Ask me anything!</h2>
           </div>
-          <div className="h-full w-full bg-white justify-between rounded-b-md flex flex-col">
-            <div>Chats</div>
-              <div className="flex justify-between border-slate-700 border-2">
-                <input 
-                type = "text"
+        </div>
+        <div className="h-full w-full bg-white justify-between rounded-b-md flex flex-col overflow-y-scroll">
+          <div className="h-full w-full flex flex-col">
+          {userInputsArray.map((input, index) => (
+            <div key={index}>{input}</div>
+          ))}
+          </div>
+          <form className="mt-auto" onSubmit={handleSubmit}>
+            <div className="flex justify-between border-slate-700 border-2">
+              <input
+                type="text"
                 value={userInput}
-                className="w-3/4"/>
-                <button onClick={handleInputChange}><FaArrowRight/></button>
-
-                {userInputsArray.map((input,index)=>
-                <div key={index}>{input}</div>)}
-              </div>
-          </div>
+                onChange={handleInputChange}
+                className="w-3/4 mt-auto"
+              />
+              <button type="submit"><FaArrowRight /></button>
+            </div>
+          </form>
+        </div>
       </div>
-
     </div>
   );
 }
